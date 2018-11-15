8ac159f00e69efa9612b5f8b6b12454a63fda573
jasarqui <jzarquilita@up.edu.ph>
2018-11-16 04:27:25 +0800

    Clean up initial commit

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Placeholder.
+      </div>
+    );
+  }
+}
+
+export default App;
